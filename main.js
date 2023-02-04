d08c7c4234b01b39b2cef70b820323eb27034793
andresropk <afrokap@gmail.com>
2023-02-04 11:39:33 -0500

    feat: ligth and grid helpers. OrbitControl

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 
 const scene = new THREE.Scene();
@@ -13,12 +14,26 @@ renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const aGeometry = new THREE.TorusGeometry(10,3,16,100);
-const aMaterial = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true})
+const aMaterial = new THREE.MeshStandardMaterial({color:0xFF6347})
 const aMesh = new THREE.Mesh(aGeometry,aMaterial)
-
-
 scene.add(aMesh)
-camera.position.setZ(30)
+
+const aLigthPoint = new THREE.PointLight(0xffffff);
+aLigthPoint.position.set(5,5,5)
+const anAmbientLight = new THREE.AmbientLight(0xffffff)
+scene.add(aLigthPoint, anAmbientLight)
+
+const aLightHelper = new THREE.PointLightHelper(aLigthPoint);
+const aGridHelper = new THREE.GridHelper(100,100)
+scene.add(aLightHelper, aGridHelper)
+
+
+camera.position.setZ(40)
+// camera.position.setY(10)
+// camera.position.setX(10)
+
+const control = new OrbitControls(camera, renderer.domElement)
+
 
 function animate(){
   requestAnimationFrame(animate)
@@ -26,6 +41,8 @@ function animate(){
   aMesh.rotateX(0.01)
   aMesh.rotateY(0.005)
   aMesh.rotateZ(0.01)
+
+  control.update()
 
   renderer.render(scene, camera)
 }
